b267de9420e8114017d1f3a2975407ed98c6b3d3
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-23 17:03:21 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(function(driverName) {
+      return driverName.toLowerCase() === string.toLowerCase();
+    });
+  }
+
+
+function fuzzyMatch(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(function(driverName) {
+      return driverName.toLowerCase().indexOf(string.toLowerCase()) === 0;
+    });
+  }
+  
+  function matchName(arrayOfObjects, string) {
+    return arrayOfObjects.filter(function(driver) {
+      return driver.name.toLowerCase() === string.toLowerCase();
+    });
+  }
